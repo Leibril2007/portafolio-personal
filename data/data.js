fb2fd8b2236a9d5b90dd1399f0ccc69eadfe18a3
Leibril <ariabi.sr@gmail.com>
2024-09-26 12:10:08 -0600

    se agrega el primer proyecto

---
+++ data/data.js
@@ -0,0 +1,11 @@
+let dataProyectos = [
+    {
+        nombre: "Proyecto Formularios",
+        descripcion: "texto, texto, texto",
+        githubPage: "https://leibril2007.github.io/Formularios/",
+        github: "https://github.com/Leibril2007/Formularios",
+        stacks: ["js","css","html"] 
+    },
+];
+
+export {dataProyectos} 
