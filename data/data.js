7cefdc3bc281f74a36f46b65529091ab18ba0e07
Leibril <ariabi.sr@gmail.com>
2024-10-01 12:48:07 -0600

    Cambio de link
    Se cambia el link del repositorio del githubPage
    y el botón github

--- data/data.js
+++ data/data.js
@@ -2,8 +2,8 @@ let dataProyectos = [
     {
         nombre: "Proyecto Formularios",
         descripcion: "texto, texto, texto",
-        githubPage: "https://leibril2007.github.io/Formularios/",
-        github: "https://github.com/Leibril2007/Formularios",
+        githubPage: "https://leibril2007.github.io/Proyectos-Formularios/",
+        github: "https://github.com/Leibril2007/Proyectos-Formularios",
         stacks: ["js","css","html"] 
     },
 ];
