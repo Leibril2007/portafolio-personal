6c8bacb4b117eb1e812408f5f0907027ae28617b
Leibril <ariabi.sr@gmail.com>
2024-10-09 21:03:53 -0600

    Actualización
    - Se actualiza el archivo data.js agregando nuevos archivos
    - Se realiza un cambio en el diseño
    - Se actualiza la página para que sea responsive, para una pantalla de 452px

--- components/project/project.js
+++ components/project/project.js
@@ -20,7 +20,7 @@ function listaProyectos(){
     // proyecto representa cadda elemento de dataProyectos
     dataProyectos.forEach( (proyecto) => {
 
-        div.appendChild(item(proyecto.nombre, proyecto.descripcion ,  proyecto.githubPage, proyecto.github, proyecto.stacks));
+        div.appendChild(item(proyecto.nombre, proyecto.descripcion ,  proyecto.githubPage, proyecto.github, proyecto.stacks, proyecto.fecha, proyecto.descargas));
 
     }); // forEach recibe como paragithubPagemetro una funcion (funcion de flecha)
 
@@ -30,37 +30,55 @@ function listaProyectos(){
 
 // FUNCION PARA LISTA
 
-function item(nombre, descripcion, githubPage, github, stacks){
+function item(nombre, descripcion, githubPage, github, stacks, fecha, descargas){
 
     let div = document.createElement('div');
     div.className = "div-item";
+
+    
+    let cuadritoProject = document.createElement('div');
+    cuadritoProject.className = 'cuadrito-project';
+    div.appendChild(cuadritoProject);
 
     let titNombre = document.createElement('h1');
     titNombre.className = "tit-Nombre";
     titNombre.innerText = nombre;
     div.appendChild(titNombre);
 
+
+    let stack = document.createElement('div');
+    stack.className = "stack-div";
+    stack.innerText = stacks;
+    div.appendChild(stack);
+
     let descipcionText = document.createElement('p');
     descipcionText.className = "descripcion-text";
     descipcionText.innerText = descripcion;
     div.appendChild(descipcionText);
 
-    let a = document.createElement('a');
-    a.className = "a-githubPage";
-    a.href = githubPage;
-    a.innerText = "githubPage";
-    div.appendChild(a);
-
-    let stack = document.createElement('div');
-    stack.className = "stack-div";
-    stack.innerText = "[js,CSS,HTML]";
-    div.appendChild(stack);
+    let fech = document.createElement('p');
+    fech.className = "fecha";
+    fech.innerText = fecha;
+    div.appendChild(fech);
 
     let botGithub = document.createElement('a');
     botGithub.className = "btn-Github";
     botGithub.href = github;
     botGithub.innerText = "Github";
     div.appendChild(botGithub);
+
+    let spanIcono = document.createElement('a');
+    spanIcono.className = "material-symbols-outlined";
+    spanIcono.innerText = "download";
+    spanIcono.href = descargas;
+    div.appendChild(spanIcono);
+
+    let enlaceDescaga = document.createElement('a');
+    enlaceDescaga.className = "icon-descarga";
+    enlaceDescaga.href = descargas;
+    spanIcono.appendChild(enlaceDescaga);
+
+
 
     return div;
 }
--- data/data.js
+++ data/data.js
@@ -1,11 +1,59 @@
 let dataProyectos = [
     {
-        nombre: "Proyecto Formularios",
-        descripcion: "texto, texto, texto",
+        nombre: "Hospital",
+        descripcion: ["Ariel Subuyuj"],
+        githubPage: "https://leibril2007.github.io/Proyectos-Formularios/",
+        fecha: "29/07/2024",
+        stacks: ["[ html, css, js ]"],
+        github: "https://github.com/Leibril2007/Hospital",
+        descargas: "https://github.com/Leibril2007/Hospital/raw/refs/heads/farmacia/src/descargas/Hospital.jar"
+    },
+    {
+        nombre: "Portafolio Personal",
+        descripcion: ["Ariel Subuyuj"],
+        githubPage: "https://leibril2007.github.io/Proyectos-Formularios/",
+        fecha: "19/09/2024",
+        stacks: ["[ html, css, js ]"],
+        github: "https://github.com/Leibril2007/portafolio-personal",
+        descargas: "https://github.com/Leibril2007/Hospital/raw/refs/heads/farmacia/src/descargas/Hospital.jar"
+    },
+    {
+        nombre: "Proyectos Formularios",
+        descripcion: ["Ariel Subuyuj"],
         githubPage: "https://leibril2007.github.io/Proyectos-Formularios/",
         github: "https://github.com/Leibril2007/Proyectos-Formularios",
-        stacks: ["js","css","html"] 
+        fecha: "08/08/2024",
+        stacks: ["[ html, css, js ]"],
+        descargas: "https://github.com/Leibril2007/Hospital/raw/refs/heads/farmacia/src/descargas/Hospital.jar"
     },
+    {
+        nombre: "Nombre proyecto",
+        descripcion: ["Ariel Subuyuj"],
+        githubPage: "https://leibril2007.github.io/Proyectos-Formularios/",
+        github: "https://github.com/Leibril2007/Proyectos-Formularios",
+        fecha: "19/09/2024",
+        stacks: ["[ html, css, js ]"],
+        descargas: "https://github.com/Leibril2007/Hospital/raw/refs/heads/farmacia/src/descargas/Hospital.jar"
+    },
+    {
+        nombre: "Nombre proyecto",
+        descripcion: ["Ariel Subuyuj"],
+        githubPage: "https://leibril2007.github.io/Proyectos-Formularios/",
+        github: "https://github.com/Leibril2007/Proyectos-Formularios",
+        fecha: "19/09/2024",
+        stacks: ["[ html, css, js ]"],
+        descargas: "https://github.com/Leibril2007/Hospital/raw/refs/heads/farmacia/src/descargas/Hospital.jar"
+    },
+    {
+        nombre: "Nombre proyecto",
+        descripcion: ["Ariel Subuyuj"],
+        githubPage: "https://leibril2007.github.io/Proyectos-Formularios/",
+        github: "https://github.com/Leibril2007/Proyectos-Formularios",
+        fecha: "19/09/2024",
+        stacks: ["[ html, css, js ]"],
+        descargas: "https://github.com/Leibril2007/Hospital/raw/refs/heads/farmacia/src/descargas/Hospital.jar"
+    },
+
 ];
 
 export {dataProyectos} 
